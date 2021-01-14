ee5519d3f73e2e378cdc5571db35615a85b3d51b
Ray Kennedy <ray.knd@gmail.com>
2021-01-14 17:17:15 -0300

    Criação da entidade vendedor

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,8 +1,10 @@
 package application;
 
+import java.util.Date;
 import java.util.Locale;
 
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
@@ -14,7 +16,10 @@ public class Program {
 		
 		Department books = new Department(1,"Books");
 		
+		Seller seller = new Seller(1, "Bob", "bob@gmail.com", new Date(), 750.00, books);
+		
 		System.out.println(books);
+		System.out.println(seller);
 
 	}
 
