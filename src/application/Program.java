57a519d67bc6b7bb64f88346fb84bbbb0ca907f7
Ray Kennedy <ray.knd@gmail.com>
2021-01-21 16:53:55 -0300

    Implementando insert

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 
@@ -30,7 +31,10 @@ public class Program {
 		seller2 = sellerDao.findAll();
 		seller2.forEach(System.out::println);
 		
-
+		System.out.println("\n\n === TEST 4: insert Seller ===");
+		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, dep);
+		sellerDao.insert(newSeller);
+		System.out.println("Inserted! new Id = " + newSeller.getId());
 	}
 
 }
